16e8e762c7cf792e065f21a6da0bfbdbe121f981
terminaldisclaimer <dan@blocknets.org>
2023-11-23 10:26:26 -0500

    update

--- code-server/main.tf
+++ code-server/main.tf
@@ -82,7 +82,7 @@ resource "coder_script" "code-server" {
     LOG_PATH : var.log_path,
     INSTALL_PREFIX : var.install_prefix,
     // This is necessary otherwise the quotes are stripped!
-    SETTINGS : replace(replace(replace(jsonencode(var.settings), "\"", "\\\""),"\"\u005D","\u005D\\\""),"\u005B\","\\\"\u005B")
+    SETTINGS : replace(replace(replace(jsonencode(var.settings), "\"", "\\\""),"\"\u005d","\u005d\\\""),"\u005b\","\\\"\u005b")
   run_on_start = true
 }
 
